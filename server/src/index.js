7faf915423a2578b435a43e61c77c2046057a489
Tejas Benibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-09-11 10:15:39 +0000

    Add: Initial Folder Structure

---
+++ server/src/index.js
@@ -0,0 +1,5 @@
+/**
+    @note : We are creating the plan on how to proceed with this project the programming part is on the hold until then
+*/
+
+console.log("We are creating the plan on how to proceed with this project the programming part is on the hold until then")
